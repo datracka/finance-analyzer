558743a19bac6505617e5980ca046a03a1ca8787
Vicens Fayos <vicensfayos@Vicenss-MacBook-Pro.local>
2024-04-12 18:17:29 +0200

    dataset added

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > pipenv shell
 
-to start the virtual environment 
+to start the virtual environment
 
 Virtual environment is managed previouly by pyenv https://realpython.com/intro-to-pyenv/
 
@@ -13,4 +13,15 @@ ollama run finance_analyzer_llama2
 
 ## to install package
 
-> pipenv install <>
+> pipenv install <whatever>
+
+## next steps
+
+- [x] You need to create your own dataset: https://huggingface.co/learn/nlp-course/chapter5/5
+
+- [ ] use it to train https://huggingface.co/docs/transformers/training
+
+- Pending, to choose the proper Pretrained model maybe this https://huggingface.co/MoritzLaurer/mDeBERTa-v3-base-mnli-xnli?
+  
+- Understand, how they connect to my local model Ollama.
+
--- categoryze_expenses.py
+++ categoryze_expenses.py
@@ -1,10 +1,19 @@
-from langchain_community.llms import Ollama
+# from langchain_community.llms import Ollama
 from dotenv import load_dotenv
+import pandas as pd
+from datasets import Dataset
+from datasets import load_dataset
+
 load_dotenv()
 
-llm = Ollama(model="llama2")
-llm.invoke("In japan, how many people speak english?")
+# llm = Ollama(model="llama2")
+# llm.invoke("In japan, how many people speak english?")
 
-import pandas as pd
-df = pd.read_csv('expenses_test.csv')
 
+df = pd.read_csv('csv/1_year_bank_statements_ING_100_only.csv')
+data_dict = df.to_dict()
+dataset = Dataset.from_dict(data_dict)
+
+# Split the dataset into train and test
+train_dataset = dataset.train_test_split(test_size=0.2)['train']
+test_dataset = dataset.train_test_split(test_size=0.2)['test']
